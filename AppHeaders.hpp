1fad7423ce5f9cf1f95a2988659775ac9c0fa69a
Administrator <administrator@chrisoldwood.com>
2003-05-15 11:02:50 +0000

    Added many new class headers.

--- AppHeaders.hpp
+++ AppHeaders.hpp
@@ -34,8 +34,18 @@
 
 // App data classes.
 #include "Servers.hpp"
+#include "Mods.hpp"
+#include "GameTypes.hpp"
+#include "FavFiles.hpp"
+#include "Favourites.hpp"
+#include "Summary.hpp"
+#include "QueryResponse.hpp"
 #include "MasterServer.hpp"
+#include "GameServer.hpp"
 #include "QueryJob.hpp"
+#include "Filter.hpp"
+#include "MasterQueryOpts.hpp"
+#include "PingOpts.hpp"
 
 // App GUI classes.
 #include "AppCmds.hpp"
